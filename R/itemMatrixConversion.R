9d0b5bc9710368ed730d4992a8b38cc1f4067455
Jiří Filip <j.f.ilip@seznam.cz>
2018-10-14 15:50:46 +0200

    first commit

---
+++ R/itemMatrixConversion.R
@@ -0,0 +1,84 @@
+as.item.matrix <- function (qcbaRuleModel, trainingData) {
+  lhs_itemsets <- c()
+  rhs_itemsets <- c()
+
+  row_number <- nrow(qcbaRuleModel@rules)
+
+  for (i in 1:row_number) {
+    row <- qcbaRuleModel@rules[i, 1]
+
+    rule_split <- strsplit(row, " => ")[[1]]
+
+    antecedent_str <- substr(rule_split[1], 2, nchar(rule_split[1]) - 1)
+    consequent_str <- substr(rule_split[2], 2, nchar(rule_split[2]) - 1)
+
+    antecedent_split <- strsplit(antecedent_str, ",")[[1]]
+
+    lhs_itemsets <- c(lhs_itemsets, antecedent_split)
+    rhs_itemsets <- c(rhs_itemsets, consequent_str)
+
+  }
+
+  lhs_itemsets_processed <- sort(unique(lhs_itemsets))
+  rhs_itemsets_processed <- sort(unique(rhs_itemsets))
+
+  lhs_rhs_len <- length(lhs_itemsets_processed) + length(rhs_itemsets_processed)
+
+  qcba_matrix_lhs <- matrix(logical(row_number * lhs_rhs_len), nrow = row_number)
+  qcba_matrix_rhs <- matrix(logical(row_number * lhs_rhs_len), nrow = row_number)
+
+  dimnames_processed <- c(lhs_itemsets_processed, rhs_itemsets_processed)
+
+  dimnames(qcba_matrix_lhs) <- list(NULL, dimnames_processed)
+  dimnames(qcba_matrix_rhs) <- list(NULL, dimnames_processed)
+
+  for (i in 1:row_number) {
+    row <- qcbaRuleModel@rules[i, 1]
+
+    rule_split <- strsplit(row, " => ")[[1]]
+
+    antecedent_str <- substr(rule_split[1], 2, nchar(rule_split[1]) - 1)
+    consequent_str <- substr(rule_split[2], 2, nchar(rule_split[2]) - 1)
+
+    antecedent_split <- strsplit(antecedent_str, ",")[[1]]
+    antecedent_processed <- sort(unique(antecedent_split))
+
+    qcba_matrix_lhs[i, antecedent_processed] <- !logical(length(antecedent_processed))
+    qcba_matrix_rhs[i, c(consequent_str)] <- TRUE
+  }
+
+  class_vals <- sapply(qcbaRuleModel@rules[,1], function (rule_string) {
+    consequent_string <- strsplit(rule_string, " => ")[[1]][2]
+    consequent_class_val <- strsplit(consequent_string, "=")[[1]][2]
+
+    substr_class_val <- substr(consequent_class_val, 1, nchar(consequent_class_val) - 1)
+
+    substr_class_val
+  })
+
+  quality_preprocessed <- qcbaRuleModel@rules[,2:3]
+  quality_preprocessed$lift <- sapply(1:row_number, function (ruleid) {
+    confidence <- quality_preprocessed$confidence[ruleid]
+    class_val <- class_vals[ruleid]
+
+    class_support <- sum(trainingData[qcbaRuleModel@classAtt] == class_val) / nrow(trainingData)
+
+    lift <- confidence / class_support
+
+    lift
+  })
+  quality_preprocessed$count <- floor(quality_preprocessed$support * nrow(trainingData))
+  quality_preprocessed$lhs_length <- apply(qcba_matrix_lhs, MARGIN = 1, FUN = sum)
+
+
+  qcba_matrix_quality <- quality_preprocessed
+
+  newRules <- new("rules",  lhs = as(qcba_matrix_lhs, "itemMatrix"),
+                  rhs = as(qcba_matrix_rhs, "itemMatrix"),
+                  quality = qcba_matrix_quality,
+                  info = list(data = "txns",
+                              ntransactions = nrow(trainingData)))
+
+  newRules
+}
+
